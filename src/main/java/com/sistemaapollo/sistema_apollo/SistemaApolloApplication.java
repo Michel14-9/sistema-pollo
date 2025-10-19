947cbfe5c13b05e85563efb158793ac899023117
Michel14-9 <duval.14-99@hotmail.com>
2025-10-19 12:12:14 -0500

    Add files via upload

---
+++ src/main/java/com/sistemaapollo/sistema_apollo/SistemaApolloApplication.java
@@ -0,0 +1,13 @@
+package com.sistemaapollo.sistema_apollo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaApolloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaApolloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistemaapollo/sistema_apollo/SistemaApolloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemaapollo.sistema_apollo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaApolloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
